c9f17e39d16459987b2ffe1d23ac40e206faf5dc
kade042 <dereje88@gmail.com>
2017-06-10 19:54:22 -0600

    Complete Search future

--- src/components/ContactsApp.js
+++ src/components/ContactsApp.js
@@ -4,16 +4,30 @@ import ContactList from './ContactList';
 import SearchBar from './SearchBar';
 
 export default class ContactsApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterText: '',
+    };
+  }
+
+  handleUserInput(searchTerm) {
+    this.setState({ filterText: searchTerm });
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <ContactList contacts={this.props.contacts} />
+        <SearchBar filterText={this.state.filterText}
+                   onUserInput={this.handleUserInput.bind(this)} />
+
+        <ContactList  contacts={this.props.contacts}
+                      filterText={this.state.filterText} />
       </div>
     );
   }
 }
 
-ContactsApp.proptypes = {
+ContactsApp.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object),
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 
 export default class SearchBar extends Component {
+  handleChange(event) {
+    this.props.onUserInput(event.target.value);
+  }
+
   render() {
-    return <input type='search' placeholder='search' />;
+    return <input type='search' placeholder='search'
+                  value={this.props.filterText}
+                  onChange={this.handleChange.bind(this)} />;
   }
 }
+
+SearchBar.propTypes = {
+  onUserInput: PropTypes.func.isRequired,
+  filterText: PropTypes.string.isRequired,
+};
